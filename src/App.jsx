673853695e8534f5ba4d359dcaf6172b6a1070d6
Sairaj182 <sairajraithatha.official@gmail.com>
2026-04-20 05:57:03 +0530

    Enabled vis count

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,58 @@ export default function App() {
 
 </a>
     {/* 👁 Visitor Counter */}
+    <div
+onClick={() => setShowTooltip((prev) => !prev)}
+  className="group fixed bottom-6 right-6 z-50
+             inline-flex items-center gap-2
+             px-4 py-2 rounded-full
+             border border-[var(--color-text)]/20
+             bg-[var(--color-bg)] shadow-md
+             hover:shadow-lg
+             transition opacity-80"
+>
 
+  {/* Eye icon */}
+  <img
+  src={dark? "/assets/vieweyedark.svg" : "/assets/vieweye.svg"}
+  alt="views"
+  className="w-4 h-4 object-contain opacity-80"
+/>
+
+  {/* Count */}
+  <span className="text-sm font-medium text-[var(--color-text)]">
+    {formatCount(displayCount)}
+  </span>
+  {/* +1 animation */}
+  {showPlus && (
+    <span
+      className="absolute -top-2 right-2 text-xs text-green-500
+                 animate-bounce pointer-events-none"
+    >
+      +1
+    </span>
+  )}
+  <div
+    className={`absolute bottom-full mb-2 left-1/2 -translate-x-1/2
+               px-2 py-1 text-xs rounded-md
+               bg-[var(--color-text)] text-[var(--color-bg)]
+               opacity-0 scale-95
+               group-hover:opacity-100 group-hover:scale-100
+               transition duration-150
+               pointer-events-none whitespace-nowrap
+               ${
+                  showTooltip
+                    ? "opacity-100 scale-100"
+                    : "opacity-0 scale-95"
+                }
+              `}
+  >
+    {visits !== null
+    ? `${visits < 10 ? "..." : visits.toLocaleString()} Views`
+    : ""}
+  </div>
+
+</div>
     </>
   );
 }
